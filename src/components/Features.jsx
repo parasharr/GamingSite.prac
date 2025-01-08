319f2a741867650285017634db965bd35dcddc8b
PranjeetGoswami <prgoswami2020@gmail.com>
2025-01-08 22:49:09 +0530

    update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -77,7 +77,7 @@ const Features = () => {
             <BentoCard 
             src="videos/feature-1.mp4"
             title={
-                <>radi<b>n</b>t</>
+                <>radia<b>n</b>t</>
             }
             desc="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
             />
